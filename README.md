47d20fe0006cfa09861a324325a0e23bb14ff9c8
rhyzx <rhyzix@gmail.com>
2016-01-11 16:30:22 +0800

    first commit 🎉

---
+++ README.md
@@ -0,0 +1,24 @@
+# vue-transfer-dom
+Transfer DOM to another place (eg. `<body>`).
+
+Useful in some situations such as z-index management, see discussion [here](https://github.com/vuejs/vue/issues/2130).
+
+
+## Demo (prevent modal overlap by other elements)
+http://jsfiddle.net/rhyzx/br5cepu3/
+
+
+## Usage
+
+```js
+Vue.use(VueTransferDom /*, {name: 'transferDom'}*/)
+
+new Vue({
+  template: '<div v-transfer-dom>foo</div>', // div will be appended to body(default)
+})
+
+// append to specific place
+new Vue({
+  template: '<div v-transfer-dom:bar>foo</div>', // div will be appended to #bar(document.getElementById)
+})
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+// umd https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+!function (root, install) {
+  if (typeof define === 'function' && define.amd) {
+    define([], { install: install })
+  } else if (typeof module === 'object' && module.exports) {
+    exports.install = install
+  } else {
+    root.VueTransferDom = { install: install }
+  }
+}(this, function (Vue, options) {
+
+var name = (options && options.name) || 'transferDom'
+name = Vue.util.hyphenate(name)
+
+var FragmentFactory = Vue.FragmentFactory
+var remove = Vue.util.remove
+var createAnchor = Vue.util.createAnchor
+
+// register as terminal directive
+Vue.compiler.terminalDirectives.push(name)
+Vue.directive(name, {
+  priority: 2000,
+  bind: function () {
+    var el = this.el
+    if (!el.__vue__) {
+      // default append to <body>
+      var container = this.arg
+        ? document.getElementById(this.arg)
+        : document.body
+      this.anchor = createAnchor('v-transfer-dom')
+      container.appendChild(this.anchor)
+      remove(this.el)
+      var factory = new FragmentFactory(this.vm, el)
+      this.frag = factory.create(this._host, this._scope, this._frag)
+      this.frag.before(this.anchor)
+    } else {
+      console.warn(
+        'v-transfer-dom="' + this.expression + '" cannot be ' +
+        'used on an instance root element.'
+      )
+    }
+  },
+  unbind: function () {
+    this.frag.remove()
+    remove(this.anchor)
+  }
+})
+
+})
